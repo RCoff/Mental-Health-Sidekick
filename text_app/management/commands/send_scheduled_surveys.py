d03570c2850fe4d60b0c7955a2fa765c0d02a42f
RCoff <enfaduraa@gmail.com>
2021-08-28 04:25:51 -0400

    Send survey schedule based on user input

--- text_app/management/commands/send_scheduled_surveys.py
+++ text_app/management/commands/send_scheduled_surveys.py
@@ -16,14 +16,21 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for user in models.UserPhoneNumber.objects.all().filter(active=True):
-            new_survey = models.ActiveSurveyStore(
-                user=user.user,
-                survey_expire_datetime=(datetime.datetime.now(datetime.timezone.utc)+datetime.timedelta(hours=12))
-            )
-            new_survey.save()
+            active_surveys = user.user.activesurveystore_set.model.objects.filter(expired_or_completed=False)
+            time_now = datetime.datetime.now(datetime.timezone.utc).time()
+            scheduled_time = user.send_survey_time
 
-            survey_id = new_survey.active_survey_id
-            base_url = os.environ['WEBSITE_HOSTNAME']
-            url = f"{base_url}form/{survey_id}"
+            if len(active_surveys) == 0:
+                if time_now >= scheduled_time:
+                    new_survey = models.ActiveSurveyStore(
+                        user=user.user,
+                        survey_expire_datetime=(datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
+                            hours=user.expire_after_hours))
+                    )
+                    new_survey.save()
 
-            send_text.send_text(url, user.phone_number)
+                    survey_id = new_survey.active_survey_id
+                    base_url = os.environ['WEBSITE_HOSTNAME']
+                    url = f"{base_url}form/{survey_id}"
+
+                    send_text.send_text(url, user.phone_number)
