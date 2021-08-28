b48e7e796fc684b7e1a0d25350990a402c9d0d07
RCoff <enfaduraa@gmail.com>
2021-08-28 15:32:19 -0400

    Update settings

--- Django_TextSurvey/settings.py
+++ Django_TextSurvey/settings.py
@@ -127,6 +127,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
