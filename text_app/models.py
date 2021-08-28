e690946370b3c87ca76fcd3efd51cefa368ca6df
RCoff <enfaduraa@gmail.com>
2021-08-28 03:48:57 -0400

    Change ResponseModel id to 1-1 field

--- text_app/models.py
+++ text_app/models.py
@@ -5,6 +5,13 @@ import uuid
 
 
 # Create your models here.
+class ActiveSurveyStore(models.Model):
+    active_survey_id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    survey_expire_datetime = models.DateTimeField(auto_now=False, blank=False, null=False)
+    expired_or_completed = models.BooleanField(default=False, choices=((True, True), (False, False),))
+
+
 class ResponseModel(models.Model):
     RESPONSE_CHOICES = (
         (1, 1),
@@ -12,8 +19,8 @@ class ResponseModel(models.Model):
         (3, 3),
     )
 
-    # id = models.OneToOneField(ActiveSurveyStore, on_delete=models.CASCADE, primary_key=True)
-    id = models.UUIDField(default=uuid.uuid4(), primary_key=True)
+    id = models.OneToOneField(ActiveSurveyStore, on_delete=models.CASCADE, primary_key=True)
+    # id = models.UUIDField(default=uuid.uuid4(), primary_key=True)
     response = models.SmallIntegerField(choices=RESPONSE_CHOICES)
     text_response = models.TextField(null=True, blank=True)
     datetime = models.DateTimeField(auto_now=True)
@@ -23,11 +30,3 @@ class UserPhoneNumber(models.Model):
     user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
     phone_number = models.PositiveBigIntegerField(blank=True, null=True)
     active = models.BooleanField(default=True, choices=((True, True), (False, False),))
-
-
-class ActiveSurveyStore(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    active_survey_id = models.UUIDField(default=uuid.uuid4(), blank=False, null=False)
-    survey_expire_datetime = models.DateTimeField(auto_now=False, blank=False, null=False)
-    expired = models.BooleanField(default=False, choices=((True, True), (False, False),))
