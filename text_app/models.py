35ac5dc411d691b1fb7760c3150a2a5cb1a67743
RCoff <enfaduraa@gmail.com>
2021-08-28 01:57:37 -0400

    Create models for tracking sent/completed surveys

--- text_app/models.py
+++ text_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 import uuid
 
 
@@ -7,9 +9,25 @@ class ResponseModel(models.Model):
     RESPONSE_CHOICES = (
         (1, 1),
         (2, 2),
+        (3, 3),
     )
 
+    # id = models.OneToOneField(ActiveSurveyStore, on_delete=models.CASCADE, primary_key=True)
     id = models.UUIDField(default=uuid.uuid4(), primary_key=True)
-    response = models.CharField(max_length=1, choices=RESPONSE_CHOICES)
+    response = models.SmallIntegerField(choices=RESPONSE_CHOICES)
+    text_response = models.TextField(null=True, blank=True)
     datetime = models.DateTimeField(auto_now=True)
 
+
+class UserPhoneNumber(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    phone_number = models.PositiveBigIntegerField(blank=True, null=True)
+    active = models.BooleanField(default=True, choices=((True, True), (False, False),))
+
+
+class ActiveSurveyStore(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4())
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    active_survey_id = models.UUIDField(default=uuid.uuid4(), blank=False, null=False)
+    survey_expire_datetime = models.DateTimeField(auto_now=False, blank=False, null=False)
+    expired = models.BooleanField(default=False, choices=((True, True), (False, False),))
