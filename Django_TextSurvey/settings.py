b82ff9656a556871bbf5e65fbe877621eae9718f
RCoff <enfaduraa@gmail.com>
2021-08-28 15:07:36 -0400

    Update settings

--- Django_TextSurvey/settings.py
+++ Django_TextSurvey/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['*']
 
