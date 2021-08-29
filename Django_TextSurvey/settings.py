68f57fb51fa611442c6c6f2587bebb731b43b35b
RCoff <enfaduraa@gmail.com>
2021-08-28 23:16:53 -0400

    Remove print statement

--- Django_TextSurvey/settings.py
+++ Django_TextSurvey/settings.py
@@ -29,7 +29,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False) == 'True'
-print(type(DEBUG))
 
 ALLOWED_HOSTS = ['*']
 
