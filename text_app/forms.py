3cd13e1494f22a1477be7e47f5c7abf8232087fb
RCoff <enfaduraa@gmail.com>
2021-08-30 23:33:15 -0400

    Add new fields to interface

--- text_app/forms.py
+++ text_app/forms.py
@@ -5,8 +5,17 @@ from . import models
 class ResponseForm(forms.ModelForm):
     class Meta:
         model = models.ResponseModel
-        fields = ['response', 'text_response']
+        fields = ['mood_response', 'hours_slept', 'daily_weight', 'daily_symptoms', 'text_response']
         widgets = {
-            'response': forms.Select(attrs={'class': 'form-control'}),
+            'mood_response': forms.NumberInput(attrs={'type': 'range',
+                                                      'class': 'form-range',
+                                                      'min': -4,
+                                                      'max': 4,
+                                                      'step': 1,
+                                                      'id': 'mood-range',
+                                                      'list': 'mood-steps'}),
+            'hours_slept': forms.NumberInput(attrs={'class': 'form-control'}),
+            'daily_weight': forms.NumberInput(attrs={'class': 'form-control'}),
+            'daily_symptoms': forms.SelectMultiple(attrs={'class': 'form-control'}),
             'text_response': forms.Textarea(attrs={'class': 'form-control'})
         }
