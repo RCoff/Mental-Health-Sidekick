845bbd8e0fa14674d42a6f9513ef5bbcba8b20b5
RCoff <enfaduraa@gmail.com>
2021-09-06 21:51:54 -0400

    Add test placeholders

--- text_app/tests.py
+++ text_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+# TODO: Create a new survey
+
+# TODO: Create a new response
+
+# TODO: Update an Old Response
+# TODO: Attempt updating a survey after it has expired
+# TODO: Attempt getting a survey that does not exist
