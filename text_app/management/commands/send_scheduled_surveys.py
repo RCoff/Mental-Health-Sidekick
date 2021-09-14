1fe71b0202ac3e1d82e481c0153561620dea66a4
RCoff <enfaduraa@gmail.com>
2021-09-13 22:24:48 -0400

    Add logging

--- text_app/management/commands/send_scheduled_surveys.py
+++ text_app/management/commands/send_scheduled_surveys.py
@@ -1,6 +1,7 @@
 import os
 import datetime
 import uuid
+import logging
 
 import pytz
 from django.core.management.base import BaseCommand, CommandError
@@ -10,18 +11,23 @@ from dotenv import load_dotenv
 from text_app import models, send_text
 
 load_dotenv()
+logger = logging.getLogger(__name__)
 
 
 # TODO: Use celery or APscheduler for scheduling?
 class Command(BaseCommand):
-    help = ''
+    help = "Sends text messages at a user's scheduled time"
 
     def handle(self, *args, **options):
         base_url = os.environ['WEBSITE_HOSTNAME']
+        start_time = datetime.datetime.now(datetime.timezone.utc)
+        logger.debug("Sending surveys texts", extra={'start_time': start_time})
 
         for user in models.UserPhoneNumber.objects.all().filter(active=True):
             active_surveys = user.user.activesurveystore_set.model.objects.filter(sent=False, expired=False,
                                                                                   completed=False, user=user.user)
+            logger.debug(f"Checking user '{user.user_id}'", extra={'user_id': user.user_id,
+                                                                   'active_surveys': len(active_surveys)})
             print(user.user.first_name)
             print(len(active_surveys))
             print(f"Next Survey At: {user.next_survey_datetime}")
@@ -29,6 +35,8 @@ class Command(BaseCommand):
 
             if len(active_surveys) == 0:
                 survey_obj = create_survey(user)
+                logger.debug("No active surveys found. Creating survey", extra={'user_id': user.user_id,
+                                                                                'survey_id': survey_obj.active_survey_id})
 
                 if user.next_survey_datetime <= datetime.datetime.now(datetime.timezone.utc):
                     survey_id = survey_obj.active_survey_id
@@ -45,6 +53,10 @@ class Command(BaseCommand):
                 else:
                     continue
             else:
+                logger.error("User has more than 1 active survey",
+                             extra={'user_id': user.user_id,
+                                    'active_surveys': len(active_surveys),
+                                    'survey_ids': active_surveys.values_list('active_survey_id', flat=True)})
                 # TODO: Raise an error
                 continue
 
@@ -53,6 +65,8 @@ class Command(BaseCommand):
                    f"Please tell me how you're feeling:\n" \
                    f"{base_url}form/{survey_id}"
 
+            logger.debug("Sending survey text", extra={'user_id': user.user_id,
+                                                       'survey_id': survey_id})
             response = send_text.send_text(body, user.phone_number)
             if response.get('sent'):
                 survey_obj.sent = True
@@ -64,7 +78,17 @@ class Command(BaseCommand):
                     datetime.datetime.now(datetime.timezone.utc).date() + datetime.timedelta(days=1),
                     survey_obj.user.userphonenumber.send_survey_time), timezone=pytz.timezone('UTC'))
                 survey_obj.user.userphonenumber.save()
+                logger.debug("Survey sent successfully", extra={'user_id': user.user_id,
+                                                                'survey_id': survey_id,
+                                                                'sent_datetime': str(survey_obj.sent_datetime)})
+            else:
+                logger.error("Failed to send survey text", extra={'user_id': user.user_id,
+                                                                  'survey_id': survey_id})
 
+        end_time = datetime.datetime.now(datetime.timezone.utc)
+        logging.info("Finished sending survey texts", extra={'start_time': start_time,
+                                                             'end_time': end_time,
+                                                             'run_time': end_time-start_time})
         return
 
 
