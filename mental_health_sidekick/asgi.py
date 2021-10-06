a230559e9b4bf2181a62a14f59998c7e4e1c5e7e
RCoff <enfaduraa@gmail.com>
2021-10-05 22:03:12 -0400

    Rename project

--- mental_health_sidekick/asgi.py
+++ mental_health_sidekick/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django_TextSurvey.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mental_health_sidekick.settings')
 
 application = get_asgi_application()
--- mental_health_sidekick/urls.py
+++ mental_health_sidekick/urls.py
@@ -1,4 +1,4 @@
-"""Django_TextSurvey URL Configuration
+"""mental_health_sidekick URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
