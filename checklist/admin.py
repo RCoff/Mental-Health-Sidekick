70c9c5a83d73c4ae56e9a17bdd3b259686dce8f6
RCoff <enfaduraa@gmail.com>
2023-07-03 12:38:19 -0400

    Register checklist models in admin

--- checklist/admin.py
+++ checklist/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    ChecklistTemplate,
+    ChecklistItemTemplate,
+    UserChecklistTemplate,
+    UserChecklistItemTemplate,
+    UserChecklist,
+    UserChecklistItem
+)
+
+
+@admin.register(ChecklistTemplate)
+class SystemChecklistTemplateAdmin(admin.ModelAdmin):
+    """ """
+
+
+@admin.register(ChecklistItemTemplate)
+class SystemChecklistItemTemplateAdmin(admin.ModelAdmin):
+    """ """
+
+
+@admin.register(UserChecklistTemplate)
+class UserChecklistTemplateAdmin(admin.ModelAdmin):
+    """ """
+
+
+@admin.register(UserChecklistItemTemplate)
+class UserChecklistItemTemplateAdmin(admin.ModelAdmin):
+    """ """
+
+
+@admin.register(UserChecklist)
+class UserChecklistAdmin(admin.ModelAdmin):
+    """ """
+
+
+@admin.register(UserChecklistItem)
+class UserChecklistItemAdmin(admin.ModelAdmin):
+    """ """
