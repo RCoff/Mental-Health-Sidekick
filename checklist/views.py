033721be19e8a8d011f98585eca03a7ac46f8790
RCoff <enfaduraa@gmail.com>
2023-07-03 19:06:12 -0400

    Add views, refactor

--- checklist/views.py
+++ checklist/views.py
@@ -1,8 +1,10 @@
 from uuid import uuid4
+import json
 
 from django.http import JsonResponse, HttpResponse
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_http_methods
 from django.shortcuts import render
+from django.views import View
 
 from .models import (
     UserChecklist,
@@ -13,7 +15,9 @@ from .models import (
 
 @require_GET
 def get_user_checklists(request):
-    user_checklists = UserChecklist.objects.filter(user=request.user)
+    user_checklists = UserChecklist.objects.filter(user=request.user,
+                                                   deleted=False) \
+        .order_by('-modified_datetime')
 
     return render(request, template_name='all_checklists.html', context={'checklists': user_checklists})
 
@@ -23,14 +27,40 @@ def get_user_checklist_templates(request):
     pass
 
 
-@require_GET
-def get_user_checklist(request, checklist_id: uuid4):
-    checklist_items = UserChecklistItem.objects.filter(deleted=False,
-                                                       user_checklist__user=request.user,
-                                                       user_checklist=checklist_id,
-                                                       user_checklist__deleted=False)
+class UserChecklistView(View):
+    def get(self, request, checklist_id: uuid4):
+        checklist_name = UserChecklist.objects.values('name').get(deleted=False,
+                                                                  user=request.user,
+                                                                  id=checklist_id)['name']
+        checklist_items = UserChecklistItem.objects.filter(deleted=False,
+                                                           user_checklist__user=request.user,
+                                                           user_checklist=checklist_id,
+                                                           user_checklist__deleted=False)
 
-    return render(request, template_name='checklist.html', context={'checklist_items': checklist_items})
+        return render(request, template_name='user_checklist.html', context={'checklist_items': checklist_items,
+                                                                             'checklist_name': checklist_name})
+
+
+class UserChecklistItemView(View):
+    def put(self, request, checklist_item_id: uuid4):
+        status = json.loads(request.body).get('status')
+        item = self._get_item(user=request.user, checklist_item_id=checklist_item_id)
+        item.status = status
+        item.save()
+
+        return HttpResponse(status=201)
+
+    def delete(self, request, checklist_item_id: uuid4):
+        item = self._get_item(user=request.user, checklist_item_id=checklist_item_id)
+        item.deleted = True
+        item.save()
+
+        return HttpResponse(status=202)
+
+    @staticmethod
+    def _get_item(user, checklist_item_id: uuid4):
+        return UserChecklistItem.objects.get(user_checklist__user=user,
+                                             id=checklist_item_id)
 
 
 @require_GET
