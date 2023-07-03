00c14612c675aa9836a7e4fe5d5b70e49dd304f0
RCoff <enfaduraa@gmail.com>
2023-07-03 19:06:58 -0400

    Update templates and URLs for checklists

--- checklist/urls.py
+++ checklist/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.get_user_checklists, name='user-checklists'),
-    path('<uuid:checklist_id>', views.get_user_checklist, name='user-checklist'),
+    path('<uuid:checklist_id>', views.UserChecklistView.as_view(), name='user-checklist'),
+    path('item/<uuid:checklist_item_id>', views.UserChecklistItemView.as_view(), name='user-checklist-item'),
     path('template/', views.get_user_checklist_templates, name='user-checklist-templates'),
     path('template/<uuid:checklist_id>', views.get_user_checklist_template, name='user-checklist-template'),
 ]
